e30c2336a565b5da8747b8ac0da5a49b980a36b4
NC.A <1792154221@qq.com>
2020-11-08 20:39:43 +0800

    [update]上传sql文件

---
+++ sql/web_plus_order_po.sql
@@ -0,0 +1,3 @@
+INSERT INTO web_plus.order_po (order_id, dead_line, item_count, item_id) VALUES ('413095', '2020-11-04 10:00:00', 2380, '3005837');
+INSERT INTO web_plus.order_po (order_id, dead_line, item_count, item_id) VALUES ('414837', '2020-11-04 12:00:00', 6260, '3059786');
+INSERT INTO web_plus.order_po (order_id, dead_line, item_count, item_id) VALUES ('416153', '2020-11-04 14:00:00', 500, '3211498');
